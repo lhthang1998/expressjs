47219aabb863611c3502ba95f850c43fa1dac819
lhthang1998 <lhthang.1998@gmail.com>
2020-05-01 20:43:10 +0700

    export task_controller

--- src/app/controller/task_controller.js
+++ src/app/controller/task_controller.js
@@ -1,4 +1,7 @@
 const Task = require('../model/task')
+const User = require('../model/user')
+const mongoose = require('mongoose')
+
 exports.tasks_get_all = async (req, res, next) => {
     try {
         let tasks = await Task.find().populate('user', 'username')
@@ -7,5 +10,84 @@ exports.tasks_get_all = async (req, res, next) => {
         })
     } catch (err) {
         return res.status(400).json(err)
+    }
+}
+
+exports.tasks_get_by_id = async (req, res, next) => {
+    try {
+        const id = req.params.id
+        let task = await Task.findById(id).populate('user', 'username  email')
+        if (!task) {
+            res.status(404).json({
+                error: "not found"
+            })
+        } else {
+            res.status(200).json(task)
+
+        }
+    } catch (err) {
+        return res.status(400).json(err)
+    }
+}
+
+exports.create_task = async (req, res, next) => {
+    try {
+        const task = new Task({
+            _id: new mongoose.Types.ObjectId(),
+            title: req.body.title,
+            user: req.body.userId,
+        })
+
+        let user = await User.find({ _id: task.userId })
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found'
+            });
+        }
+
+        let create = await task.save()
+        if (!create) {
+            return res.status(400).json({
+                error: "bad request"
+            })
+        }
+        res.status(200).json(create)
+    } catch (err) {
+        return res.status(400).json(err)
+    }
+}
+
+exports.update_task = async (req, res, next) => {
+    try {
+        const id = req.params.id
+
+        const isReturnNewDocs = { new: true, useFindAndModify: false }
+        const updateOpts = new Task({
+            title: req.body.title
+        })
+        let task = await Task.findOneAndUpdate({ _id: id }, { $set: updateOpts }, isReturnNewDocs).populate('user', 'username')
+        if (!task) {
+            return res.status(404).json({
+                error: "not found"
+            })
+        }
+        res.status(200).json(task)
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+}
+
+exports.delete_task = async (req, res) => {
+    try {
+        const id = req.params.id
+        let task = await Task.findByIdAndDelete({ _id: id }).populate('user', 'username')
+        if (!task) {
+            return res.status(404).json({
+                error: "not found"
+            })
+        }
+        res.status(200).json(task)
+    } catch (err) {
+        return res.status(500).json(err)
     }
 }
--- src/app/router/task.js
+++ src/app/router/task.js
@@ -3,87 +3,17 @@ const router = express.Router()
 const Task = require('../model/task')
 const User = require('../model/user')
 const mongoose = require('mongoose')
-const TaskController  = require('../controller/task_controller')
+const TaskController = require('../controller/task_controller')
 
 router.get('/', TaskController.tasks_get_all)
 
-router.get('/:id', async (req, res, next) => {
-    try {
-        const id = req.params.id
-        let task = await Task.findById(id).populate('user', 'username  password')
-        if (!task) {
-            res.status(404).json({
-                error: "not found"
-            })
-        } else {
-            res.status(200).json(task)
+router.get('/:id', TaskController.tasks_get_by_id)
 
-        }
-    } catch (err) {
-        return res.status(400).json(err)
-    }
-})
+router.post('/', TaskController.create_task)
 
-router.post('/', async (req, res, next) => {
-    try {
-        const task = new Task({
-            _id: new mongoose.Types.ObjectId(),
-            title: req.body.title,
-            user: req.body.userId,
-        })
+router.put('/:id', TaskController.update_task)
 
-        let user = await User.find({ _id: task.userId })
-        if (!user) {
-            return res.status(404).json({
-                message: 'User not found'
-            });
-        }
-        let create = await task.save()
-        if (!create) {
-            return res.status(400).json({
-                error: "bad request"
-            })
-        }
-        res.status(200).json(create)
-    } catch (err) {
-        return res.status(400).json(err)
-    }
-})
-
-router.put('/:id', async (req, res, next) => {
-    try {
-        const id = req.params.id
-
-        const isReturnNewDocs = { new: true, useFindAndModify: false }
-        const updateOpts = new Task({
-            title: req.body.title
-        })
-        let task = await Task.findOneAndUpdate({ _id: id }, { $set: updateOpts }, isReturnNewDocs).populate('user', 'username')
-        if (!task) {
-            return res.status(404).json({
-                error: "not found"
-            })
-        }
-        res.status(200).json(task)
-    } catch (err) {
-        return res.status(500).json(err)
-    }
-})
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const id = req.params.id
-        let task = await Task.findByIdAndDelete({ _id: id }).populate('user', 'username')
-        if (!task) {
-            return res.status(404).json({
-                error: "not found"
-            })
-        }
-        res.status(200).json(result)
-    } catch (err) {
-        return res.status(500).json(err)
-    }
-})
+router.delete('/:id', TaskController.delete_task)
 
 
 module.exports = router
